3509fc12a0339b40b0db2631b89069f6b6ae25bd
Brian Bancroft <hello@brianbancroft.ca>
2020-08-15 10:02:24 -0700

    ✨  Create layer action on mouse enter and leave

--- src/components/Map.js
+++ src/components/Map.js
@@ -87,6 +87,34 @@ const MapboxGLMap = ({ vehicles }) => {
             'circle-color': colorMap[vehicles[i].properties.vehicleType],
           },
         })
+
+        map.on('mouseenter', id, function (e) {
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = 'pointer'
+
+          var coordinates = e.features[0].geometry.coordinates.slice()
+          var vehicleType = e.features[0].properties.vehicleType
+
+          console.log('hi hi hi')
+
+          console.log('Vehicle type ', vehicleType)
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+          // }
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          // popup.setLngLat(coordinates).setHTML(description).addTo(map)
+        })
+
+        map.on('mouseleave', id, function () {
+          map.getCanvas().style.cursor = ''
+          // popup.remove()
+        })
       }
     }
 
@@ -115,6 +143,34 @@ const MapboxGLMap = ({ vehicles }) => {
             'circle-radius': 10,
             'circle-color': colorMap[vehicles[i].properties.vehicleType],
           },
+        })
+
+        map.on('mouseenter', id, function (e) {
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = 'pointer'
+
+          var coordinates = e.features[0].geometry.coordinates.slice()
+          var vehicleType = e.features[0].properties.vehicleType
+
+          console.log('hi hi hi')
+
+          console.log('Vehicle type ', vehicleType)
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+          // }
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          // popup.setLngLat(coordinates).setHTML(description).addTo(map)
+        })
+
+        map.on('mouseleave', id, function () {
+          map.getCanvas().style.cursor = ''
+          // popup.remove()
         })
       } else {
         map.getSource(id).setData(vehicles[i])
